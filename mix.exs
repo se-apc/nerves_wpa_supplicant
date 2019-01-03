6ea671d0a3eeac2df8e7b335809a14faffeb3aab
connor rigby <konnorrigby@gmail.com>
2019-01-03 09:03:15 -0800

    Migrate to CircleCI

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule NervesWpaSupplicant.Mixfile do
   defp deps do
     [
       {:elixir_make, "~> 0.4", runtime: false},
-      {:ex_doc, "~> 0.18.3", only: :dev}
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 
